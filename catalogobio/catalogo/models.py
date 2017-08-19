555df752cfd003113467f946f5a66fa8d6bc3db1
Rafael Bustamante <r.bustamante32@uniandes.edu.co>
2017-08-19 18:14:57 -0500

    Agregado modelo para especies

--- catalogobio/catalogo/models.py
+++ catalogobio/catalogo/models.py
@@ -2,7 +2,12 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+CLASIFICATION_CHOICES = (
+    ('1','1'),
+    ('2','2'),
+    ('3','3'),
+)
+
 class UserProfile(models.Model):
 
    user = models.OneToOneField(User)
@@ -10,3 +15,17 @@ class UserProfile(models.Model):
    city = models.CharField(max_length=256, blank=False, null=False)
    interests = models.CharField(max_length=256, blank=False, null=False)
 
+class Categoria(models.Model):
+   nombre = models.CharField(max_length=256, blank=False, null=False)
+
+class Especie(models.Model):
+    nombre = models.CharField(max_length=256, blank=False, null=False)
+    clasificacion = CLASIFICATION_CHOICES
+    nombre_cientifico = models.CharField(max_length=256, blank=False, null=False)
+    categoria = models.ForeignKey(Categoria)
+    foto = models.CharField(max_length=256, blank=False, null=False)
+    descripcion = models.CharField(max_length=256, blank=False, null=False)
+
+class Comentario(models.Model):
+    email = models.CharField(max_length=256, blank=False, null=False)
+    especie = models.ForeignKey(Especie)
