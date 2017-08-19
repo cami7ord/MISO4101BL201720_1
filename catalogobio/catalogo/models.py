94c03c8ac3305ca33583d9c8b5efa8bc39f594f8
Erick Coral <julian@MacBook-Air-de-Erick.local>
2017-08-19 12:57:35 -0500

    Catalogo Biologico. Admin Section -  Custom User Table added

---
+++ catalogobio/catalogo/models.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+class UserProfile(models.Model):
+
+   user = models.OneToOneField(User)
+   country = models.CharField(max_length=256, blank=False, null=False)
+   city = models.CharField(max_length=256, blank=False, null=False)
+   interests = models.CharField(max_length=256, blank=False, null=False)
+
