51d4294784bf19463c4d45a448e4b5e9aa07ec3f
Rafael Bustamante <r.bustamante32@uniandes.edu.co>
2017-08-20 12:24:24 -0500

    Added comment model

--- catalogobio/catalogo/admin.py
+++ catalogobio/catalogo/admin.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from .models import UserProfile
 from .models import Category
 from .models import Species
+from .models import Comment
 
 
 
@@ -14,9 +15,17 @@ class UserProfileAdmin(admin.ModelAdmin):
         return instance.user.username
 
 class SpeciesAdmin(admin.ModelAdmin):
-    list_display = ('name', 'scientific_name', 'category', 'description')
+    list_display = (
+        'name',
+        'scientific_name',
+        'category',
+        'description',
+                    )
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('species', 'email', 'text')
 
 admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(Species, SpeciesAdmin)
 admin.site.register(Category)
+admin.site.register(Comment, CommentAdmin)
