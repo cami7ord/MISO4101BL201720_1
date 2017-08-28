e524feb12d07084e9c45109841bf3922675fef82
Camilo Baquero <cami7ord@gmail.com>
2017-08-28 04:14:14 +0000

    Added the login func

--- catalogobio/catalogo/urls.py
+++ catalogobio/catalogo/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'catalogo'
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^species_create/$',views.species_create, name='addspecies'),
     url(r'^species_view/$',views.species_view, name='viewspecies'),
+    url(r'^login/$', views.login_view, name='login'),
+    url(r'^logout/$', views.logout_view, name='logout'),
 ]
